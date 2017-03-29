e084defbf4f3b447079eb7f667029926a99fd2d5
Wynter Woods <zerotri@gmail.com>
2017-03-29 12:07:41 -0700

    Example test of framework

--- src/game-test/main.c
+++ src/game-test/main.c
@@ -1,9 +1,19 @@
-#include <2d>
+#include <engine>
 
 int main( int argc, char** argv ) {
-	game_window *window = NULL;
-	window_create("Testing", 640, 480, &window);
-	while(1) {
+	window_t window = NULL;
+	
+	pixel_buffer_t buffer = calloc(640 * 480, sizeof(int));
+	memset(buffer, 0xff, 640*480*4);
+
+	if(window_create(&window, "Testing", 640, 480)) {
+		printf("Failure\n");
+		return 1;
 	}
+	while(!window_received_quit_event(window)) {
+		window_update_events(window);
+		window_update_buffer(window, buffer);
+	}
+	window_destroy(window);
 	return 0;
 }
